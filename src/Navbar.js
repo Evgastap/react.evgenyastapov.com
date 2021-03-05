7b25f52961fd2a0df46e5013108d9684aa8d88a3
Evgastap <evgastap@gmail.com>
2021-03-05 15:06:32 +0100

    adding framer motion

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,14 @@ import { faBars, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { React, Component } from 'react'
 import { ReactComponent as Logo} from "./logo.svg"
+import { motion } from "framer-motion"
+
+const menuItemMotion = {
+    scale: 1.1, opacity : 0.8 
+}
 
 export default class Navbar extends Component {
+
     constructor(props) {
         super(props)
         this.state = { showMenu: false }
@@ -18,20 +24,26 @@ export default class Navbar extends Component {
     render = () => {
         return (
             <>
-            <div className={this.state.showMenu ? "fixed top-0 w-full h-full bg-gradient-to-tr from-purple-700 to-red-600 flex items-center" : "hidden"}>
+            <motion.div 
+            className={this.state.showMenu ? 
+                "fixed top-0 w-full h-full bg-gradient-to-tr from-purple-700 to-red-600 flex items-center" : 
+                "hidden"
+            }
+            animate={this.state.showMenu ? { opacity : 1 } : { opacity: 0 }}
+            >
                 <ul className="text-3xl font-medium text-white tracking-wider leading-relaxed text-center w-full">
-                        <li>Portfolio</li>
+                        <motion.li whileHover={ this.menuItemMotion }><a href="#">Portfolio</a></motion.li>
                         <li>About</li>
                         <li>Instagram</li>
                         <li>Contact</li>
                         <li className="pt-5">
-                            <FontAwesomeIcon icon={faEnvelopeOpen} /><span className="px-2"></span>
-                            <FontAwesomeIcon icon={faInstagram}/><span className="px-2"></span>
-                            <FontAwesomeIcon icon={faLinkedinIn}/><span className="px-2"></span>
-                            <FontAwesomeIcon icon={faYoutube}/>
+                            <FontAwesomeIcon icon={faEnvelopeOpen} className="mx-4"/>
+                            <FontAwesomeIcon icon={faInstagram} className="mx-4"/>
+                            <FontAwesomeIcon icon={faLinkedinIn} className="mx-4"/>
+                            <FontAwesomeIcon icon={faYoutube} className="mx-4"/>
                         </li>
                 </ul>
-            </div>
+            </motion.div>
             <div className="fixed flex w-full px-10 pt-5 justify-between top-0">
                 <div><Logo className="h-10"/></div>
                 <div><FontAwesomeIcon icon={faBars} className="fill-current text-white w-10 text-3xl" onClick={this.menuShowToggle} /></div>
